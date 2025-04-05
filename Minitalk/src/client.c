f07c575293474f9c88d445b022cb19e666e09ad9
ROBLOXHACCKER <emailcreatasulmomentoo@gmail.com>
2025-04-05 22:20:49 +0200

    client finito

--- Minitalk/src/client.c
+++ Minitalk/src/client.c
@@ -6,11 +6,29 @@
 /*   By: hurasmi <hurasmi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 17:26:20 by hurasmi           #+#    #+#             */
-/*   Updated: 2025/04/03 09:55:36 by hurasmi          ###   ########.fr       */
+/*   Updated: 2025/04/05 22:20:25 by hurasmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/mini-fun/minitalk.h"
+
+void char_to_binary(char c, int pid_server)
+{
+    int i = 7;
+    printf("\nLETTERA %c\n", c);
+    while(i >= 0)
+    {
+        if((c >> i) & 1)
+        {
+            kill(pid_server, SIGUSR2);
+        }
+        else
+        {
+            kill(pid_server, SIGUSR1);
+        }
+        i--;
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -19,12 +37,15 @@ int main(int argc, char *argv[])
         printf("Errore: parametri input sbagliati");
         return -1;
     }
+    int server_pid = ft_atoi(argv[1]);
+    char *message_to_server;
 
-    
-    
-
-
-    
+    message_to_server = argv[2];
+    while(*message_to_server != '\0')
+    {
+        char_to_binary(*message_to_server, server_pid);
+        message_to_server++;
+    }   
 
     return 0;
 }
--- Minitalk/src/server.c
+++ Minitalk/src/server.c
@@ -6,11 +6,26 @@
 /*   By: hurasmi <hurasmi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 17:26:24 by hurasmi           #+#    #+#             */
-/*   Updated: 2025/04/03 09:55:27 by hurasmi          ###   ########.fr       */
+/*   Updated: 2025/04/05 21:54:02 by hurasmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/mini-fun/minitalk.h"
+
+
+int s_return(int sig)
+{
+    if(sig == SIGUSR1)
+    {
+        return 0;
+    }else if(sig == SIGUSR2)
+    {
+        return 1;
+    }else
+    {
+        exit(-1);
+    }
+}
 
 int main()
 {
@@ -23,8 +38,7 @@ int main()
     {
         ft_printf("SERVER PID: %s", ft_itoa((int)server_pid));
     }
-    
-    
+
     
     
     while(1)
