ff2f4bd1aa509c2b771db78fde99b5e8cc0414ce
emailcreatasulmomentoo@gmail.com <emailcreatasulmomentoo@gmail.com>
2025-04-28 11:34:09 +0200

    aggiunto contatore movimenti

--- so_long/include/key_menager.c
+++ so_long/include/key_menager.c
@@ -2,18 +2,35 @@
 
 int key_hook(int keycode, t_game *g)
 {
+    static int move_count = 0;
     t_player *p = &(g->player);
 
     if (keycode == 53) // ESC
         exit(0);
     else if (keycode == 123 && g->map[p->y][p->x - 1] != '1') // sinistra
+    {
         p->x--;
+        move_count++;
+        printf("%d\n", move_count);
+    }
     else if (keycode == 124 && g->map[p->y][p->x + 1] != '1') // destra
+    {
         p->x++;
+        move_count++;
+        printf("%d\n", move_count);
+    }
     else if (keycode == 125 && g->map[p->y + 1][p->x] != '1') // giù
+    {
         p->y++;
+        move_count++;
+        printf("%d\n", move_count);
+    }
     else if (keycode == 126 && g->map[p->y - 1][p->x] != '1') // su
+    {
         p->y--;
+        move_count++;
+        printf("%d\n", move_count);
+    }
 
     render(g, p, &(g->block));
     return 0;
